4ba266c30a4512b83bfbf82faac39d8652a77fea
Sergei Voinarenko <voynarenko@gmail.com>
2025-03-08 18:19:00 +0300

    Swinging pendulum added.

---
+++ Assets/Code/Gameplay/Features/Circle/CircleColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Gameplay.Features.Circle
+{
+  public class CircleColor : MonoBehaviour
+  {
+    [SerializeField] private List<Color> _colors;
+    [SerializeField] private SpriteRenderer _renderer;
+    
+    private void Awake() =>
+      SetColor();
+
+    private void SetColor()
+    {
+      if (_renderer) 
+        _renderer.color = _colors[Random.Range(0, _colors.Count)];
+    }
+  }
+}
---
+++ Assets/Code/Gameplay/Features/Pendulum/PendulumSwing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Code.Gameplay.Features.Pendulum
+{
+  public class PendulumSwing : MonoBehaviour
+  {
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _amplitude = 45f;
+    private float _angle;
+
+    private void Update()
+    {
+      _angle = Mathf.Sin(Time.time * _speed) * _amplitude;
+      transform.rotation = Quaternion.Euler(0, 0, _angle);
+    }
+  }
+}
